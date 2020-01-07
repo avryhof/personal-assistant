57e5ad62a1a19cc35d08dc93c972eb96adf58aa8
Amos Vryhof <amos@vryhofresearch.com>
2020-01-07 15:59:36 -0500

    fixed module_name variable

--- model_helpers.py
+++ model_helpers.py
@@ -1,7 +1,7 @@
 import importlib
 
 
-def lazy_load_class(app_name):
-    loaded_module = importlib.import_module(mudule_name)
+def lazy_load_class(module_name):
+    loaded_module = importlib.import_module(module_name)
 
     return loaded_module
