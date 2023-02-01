c1e778d27c82851cecc61a5eed2f78ff6882bbd2
Amos Vryhof <avryhof@gmail.com>
2023-02-01 18:39:12 -0500

    Integrate ChatGPT

--- settings.py
+++ settings.py
@@ -1,22 +1,30 @@
 import os
 from pathlib import Path
 
+from tts_wrapper import PicoClient, SAPIClient, SAPITTS
+
 from personal_assistant.constants import RECOGNIZER_WIT
 
 BASE_DIR = Path(__file__).resolve().parent
+
+# client = PicoClient()
+client = SAPIClient()
+tts = SAPITTS(client)
+
+DEBUG = False
+
+DEAF = True
+DUMB = True
 
 # RECOGNITION_ENGINE = RECOGNIZER_SNOWBOY
 # RECOGNITION_ENGINE = RECOGNIZER_SPHINX
 RECOGNITION_ENGINE = RECOGNIZER_WIT
 
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+# BASE_RESPONDER = "chatgpt"
+BASE_RESPONDER = "nltk"
+
 WIT_AI_KEY = os.environ.get("WIT_ACCESS_TOKEN")
-
-TUYA_CLIENT_ID = "acmn8staa0kr86vyadd4"
-TUYA_CLIENT_SECRET = "f5d87339955b451f9ba0e27de2089717"
-TUYA_CHANNEL_ID = "amostest"
-
-SUBSONIC_URL = "http://subsonic.vryhof.net:4040"
-SUBSONIC_USER = "googlehome"
-SUBSONIC_PASSWORD = "googlehome"
 
 SKILLS_REGISTRY = []
